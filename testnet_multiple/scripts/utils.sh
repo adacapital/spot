227c4bfca0f35e95afd7e1c9c636cef0214abad9
Thomas Langlois <thomas.langlois@mac.com>
2021-07-19 06:12:54 +0000

    fix

--- testnet_multiple/scripts/utils.sh
+++ testnet_multiple/scripts/utils.sh
@@ -69,7 +69,7 @@ get_topo () {
                 TOPO_IP_PUB=$(awk '{ print $3 }' <<< "${TOPO}")
                 #echo "TOPO_IP: ${TOPO_IP}"
                 #echo "TOPO_NAME: ${TOPO_NAME}"
-                if [[ $TOPO_NAME == "bp" ]]; then:
+                if [[ $TOPO_NAME == "bp" ]]; then
                     BP_IP=$TOPO_IP
                 fi
 
