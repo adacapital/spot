0245d864cafbc3774f12bac0fc4dc9aa87722ed6
Thomas Langlois <thomas.langlois@mac.com>
2021-08-07 14:31:29 +0000

    script clean up + cncli install + leaderlog

--- testnet_multiple/install/init_cncli.sh
+++ testnet_multiple/install/init_cncli.sh
@@ -1,25 +1,62 @@
 #!/bin/bash
 # Only relevant for block producing node
 
-# Installing CNCLI binary
-RELEASETAG=$(curl -s https://api.github.com/repos/AndrewWestberg/cncli/releases/latest | jq -r .tag_name)
-VERSION=$(echo ${RELEASETAG} | cut -c 2-)
-echo "Installing CNCLI binary release ${RELEASETAG}"
-mkdir -p $HOME/download/cncli
-curl -sLJ https://github.com/AndrewWestberg/cncli/releases/download/${RELEASETAG}/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz -o $HOME/download/cncli/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
+# global variables
+now=`date +"%Y%m%d_%H%M%S"`
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+SPOT_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
+NS_PATH="$SPOT_DIR/scripts"
+TOPO_FILE=~/pool_topology
 
-sudo tar xzvf $HOME/download/cncli/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz -C /usr/local/bin/
+# importing utility functions
+source $NS_PATH/utils.sh
 
-echo "Checking installed version:"
-cncli -V
+echo
+echo '---------------- Reading pool topology file and preparing a few things... ----------------'
 
-echo "Setting up CNCLI SYNC as a service"
-BLOCKPRODUCING_IP="127.0.0.1"
-BLOCKPRODUCING_PORT=3000
+read ERROR NODE_TYPE BP_IP RELAYS < <(get_topo $TOPO_FILE)
+RELAYS=($RELAYS)
+cnt=${#RELAYS[@]}
+let cnt1="$cnt/3"
+let cnt2="$cnt1 + $cnt1"
+let cnt3="$cnt2 + $cnt1"
 
-mkdir -p $HOME/node.bp/cncli
+RELAY_IPS=( "${RELAYS[@]:0:$cnt1}" )
+RELAY_NAMES=( "${RELAYS[@]:$cnt1:$cnt1}" )
+RELAY_IPS_PUB=( "${RELAYS[@]:$cnt2:$cnt1}" )
 
-cat > $HOME/node.bp/cncli/cncli_sync.service << EOF
+if [[ $ERROR == "none" ]]; then
+    echo "NODE_TYPE: $NODE_TYPE"
+    echo "RELAY_IPS: ${RELAY_IPS[@]}"
+    echo "RELAY_NAMES: ${RELAY_NAMES[@]}"
+    echo "RELAY_IPS_PUB: ${RELAY_IPS_PUB[@]}"
+else
+    echo "ERROR: $ERROR"
+    exit 1
+fi
+
+if [[ $NODE_TYPE == "bp" ]]; then
+    echo
+    echo '---------------- Installing CNCLI binary ----------------'
+
+    RELEASETAG=$(curl -s https://api.github.com/repos/AndrewWestberg/cncli/releases/latest | jq -r .tag_name)
+    VERSION=$(echo ${RELEASETAG} | cut -c 2-)
+    echo "Installing CNCLI binary release ${RELEASETAG}"
+    mkdir -p $HOME/download/cncli
+    curl -sLJ https://github.com/AndrewWestberg/cncli/releases/download/${RELEASETAG}/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz -o $HOME/download/cncli/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz
+
+    sudo tar xzvf $HOME/download/cncli/cncli-${VERSION}-x86_64-unknown-linux-gnu.tar.gz -C /usr/local/bin/
+
+    echo "Checking installed version:"
+    cncli -V
+
+    echo "Setting up CNCLI SYNC as a service"
+    BLOCKPRODUCING_IP=$BP_IP
+    BLOCKPRODUCING_PORT=3000
+
+    mkdir -p $HOME/node.bp/cncli
+
+    cat > $HOME/node.bp/cncli/cncli_sync.service << EOF
 [Unit]
 Description=CNCLI Sync
 After=multi-user.target
@@ -41,10 +78,13 @@ SyslogIdentifier=cncli_sync
 WantedBy=multi-user.target
 EOF
 
-sudo mv $HOME/node.bp/cncli/cncli_sync.service /etc/systemd/system/cncli_sync.service
+    sudo mv $HOME/node.bp/cncli/cncli_sync.service /etc/systemd/system/cncli_sync.service
 
-sudo systemctl daemon-reload
-sudo systemctl enable cncli_sync
-sudo systemctl start cncli_sync.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable cncli_sync
+    sudo systemctl start cncli_sync.service
 
-# note cncli service logs can be seen in /var/log/syslog
+    # note cncli service logs can be seen in /var/log/syslog
+else
+    echo "You should only install cncli on your BP node."
+fi
