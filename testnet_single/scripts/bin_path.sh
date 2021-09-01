14cc090b9a23e542eea7fe0a6e1746793b660939
Thomas Langlois <thomas.langlois@mac.com>
2021-09-01 18:39:11 +0000

    alonzo upgrade tweaks to testnet single

--- testnet_single/scripts/bin_path.sh
+++ testnet_single/scripts/bin_path.sh
@@ -2,4 +2,4 @@
 
 if [[ $# -eq 1 && ! $1 == "" ]]; then BIN_NAME=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 {binaryName}"; exit 2; fi
 
-echo "$(jq -r '."install-plan"[] | select(."component-name" == "exe:'$BIN_NAME'") | ."bin-file"' ~/download/cardano-node/dist-newstyle/cache/plan.json | head -n 1)"
+echo "$(jq -r '."install-plan"[] | select(."component-name" == "exe:'$BIN_NAME'") | ."bin-file"' $HOME/download/cardano-node/dist-newstyle/cache/plan.json | head -n 1)"
