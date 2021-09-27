39fb7ad0eca290e3d8415e215453c2ef9962711b
Thomas Langlois <thomas.langlois@mac.com>
2021-09-27 21:07:11 +0000

    tweaks

--- testnet_single/scripts/update_node_binaries.sh
+++ testnet_single/scripts/update_node_binaries.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-if [[ $# -eq 1 && ! $1 == "" ]]; then nodeName=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 {versionTag}"; exit 2; fi
+# global variables
+NOW=`date +"%Y%m%d_%H%M%S"`
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+SPOT_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
+NS_PATH="$SPOT_DIR/scripts"
+
+# importing utility functions
+source $NS_PATH/utils.sh
+
+if [[ $# -eq 1 && ! $1 == "" ]]; then nodeName=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 \"{versionTag}\""; exit 2; fi
 
 sudo unattended-upgrade -d
 sudo apt-get update -y
 sudo apt-get upgrade -y
+
+cardano-cli --version
 
 echo
 echo '---------------- Updating the node from source ---------------- '
 cd ~/download/cardano-node
 git fetch --all --tags
 git checkout "tags/$1"
+
+git describe --tags
+
+if ! promptyn "Is this the correct tag? (y/n)"; then
+    echo "Ok bye!"
+    exit 1
+fi
 
 cabal clean
 cabal update
