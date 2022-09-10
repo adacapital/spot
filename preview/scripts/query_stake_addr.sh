b0adb4477b42c48a1ea24c7cc63805b87e21ab1a
Thomas Langlois <thomas.langlois@mac.com>
2022-09-10 10:42:46 +0000

    tweak

--- preview/scripts/query_stake_addr.sh
+++ preview/scripts/query_stake_addr.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# global variables
+now=`date +"%Y%m%d_%H%M%S"`
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+SPOT_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
+NS_PATH="$SPOT_DIR/scripts"
+
+# importing utility functions
+source $NS_PATH/utils.sh
+MAGIC=$(get_network_magic)
+
 if [[ $# -ge 1 && ! $1 == "" ]]; then STAKE_ADDR=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 {stake_addr}"; exit 2; fi
 
-cardano-cli query stake-address-info --address $STAKE_ADDR --testnet-magic 1097911063
+cardano-cli query stake-address-info --address $STAKE_ADDR --testnet-magic $MAGIC
