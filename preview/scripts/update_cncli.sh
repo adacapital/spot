14e1b254de15f2a3503fe8de1f201038626204da
Thomas Langlois <thomas.langlois@mac.com>
2022-09-26 14:00:35 +0000

    tweak

--- preview/scripts/update_cncli.sh
+++ preview/scripts/update_cncli.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Only relevant for block producing node
 
+# global variables
+now=`date +"%Y%m%d_%H%M%S"`
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+SPOT_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
+NS_PATH="$SPOT_DIR/scripts"
+TOPO_FILE=~/pool_topology
+
+# importing utility functions
+source $NS_PATH/utils.sh
+
 CNCLI_PATH=`which cncli`
 if [[ $CNCLI_PATH == "" ]]; then
     echo "CNCLI does not seem to be installed. Please run init_cncli first."
