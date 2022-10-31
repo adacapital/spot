db7d5224b330bb9ccb8230d13cf89041877c8aa1
Thomas Langlois <thomas.langlois@mac.com>
2022-10-31 16:26:53 +0000

    cncli update step TODO

--- preprod/scripts/respin_node.sh
+++ preprod/scripts/respin_node.sh
@@ -46,50 +46,53 @@ fi
 
 # respining the nodes
 if [[ $NODE_TYPE == "bp" ]]; then
-    # echo
-    # echo '---------------- Purging db ----------------'
-    # if ! promptyn "Proceed with purging the node's db? (y/n)"; then
-    #     echo "Ok bye for now!"
-    #     exit 1
-    # fi
+    echo
+    echo '---------------- Purging db ----------------'
+    if ! promptyn "Proceed with purging the node's db? (y/n)"; then
+        echo "Ok bye for now!"
+        exit 1
+    fi
 
-    # # purging the node's db
-    # rm -rf $HOME/node.bp/db/*
-    # rm -f $HOME/node.bp/cncli/cncli.db*
-    # rm -rf $HOME/node.bp/logs
-    # echo "DB purge done!"
+    # purging the node's db
+    rm -rf $HOME/node.bp/db/*
+    rm -f $HOME/node.bp/cncli/cncli.db*
+    rm -rf $HOME/node.bp/logs
+    echo "DB purge done!"
 
-    # echo
-    # echo '---------------- Backing up config files ----------------'
-    # if ! promptyn "Proceed with backing up config files? (y/n)"; then
-    #     echo "Ok bye for now!"
-    #     exit 1
-    # fi
+    echo
+    echo '---------------- Backing up config files ----------------'
+    if ! promptyn "Proceed with backing up config files? (y/n)"; then
+        echo "Ok bye for now!"
+        exit 1
+    fi
 
-    # mkdir $HOME/node.bp/config/bkup_$NOW
-    # cp $HOME/node.bp/config/*.json $HOME/node.bp/config/bkup_$NOW
+    mkdir $HOME/node.bp/config/bkup_$NOW
+    cp $HOME/node.bp/config/*.json $HOME/node.bp/config/bkup_$NOW
 
-    # mkdir -p $HOME/node.bp/config/config_master
-    # rm -f $HOME/node.bp/config/config_master/*
-    # cd $HOME/node.bp/config/config_master
+    mkdir -p $HOME/node.bp/config/config_master
+    rm -f $HOME/node.bp/config/config_master/*
+    cd $HOME/node.bp/config/config_master
 
-    # wget -O config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/config.json
-    # wget -O bgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/byron-genesis.json
-    # wget -O sgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/shelley-genesis.json
-    # wget -O agenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/alonzo-genesis.json
-    # wget -O topology.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/topology.json
-    # wget -O db-sync-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/db-sync-config.json
-    # wget -O submit-api-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/submit-api-config.json
+    wget -O config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/config.json
+    wget -O bgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/byron-genesis.json
+    wget -O sgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/shelley-genesis.json
+    wget -O agenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/alonzo-genesis.json
+    wget -O topology.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/topology.json
+    wget -O db-sync-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/db-sync-config.json
+    wget -O submit-api-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/submit-api-config.json
  
-    # echo "Config file backup done!"
+    echo "Config file backup done!"
 
-    # echo
-    # echo "Now please compare changes between config_master config files and live ones and promote manually the relevant changes to the config files in $HOME/node.bp/config."
+    echo
+    echo "Now please compare changes between config_master config files and live ones and promote manually the relevant changes to the config files in $HOME/node.bp/config."
+    echo
+    echo "Warning: if sgenesis.json changed you will have to change  --shelley-genesis-hash in /etc/systemd/system/cncli_sync.service to the new hash, then run sudo systemctl daemon-reload for the change to take effect!"
+    #TODO automate this cncli config step
 
-    # if ! promptyn "Ready to proceed? (y/n)"; then
-    #     echo "Ok bye for now!"
-    #     exit 1
-    # fi
+    if ! promptyn "Ready to proceed? (y/n)"; then
+        echo "Ok bye for now!"
+        exit 1
+    fi
 
     echo
     echo '---------------- Working on relays ----------------'
@@ -100,27 +103,27 @@ if [[ $NODE_TYPE == "bp" ]]; then
 
     RELAYS_COUNT=${#RELAY_IPS[@]}
 
-    # for (( i=0; i<${RELAYS_COUNT}; i++ ));
-    # do
-    #     echo "Checking ${RELAY_IPS[$i]} is online..."
-    #     ping -c1 -W1 -q ${RELAY_IPS[$i]} &>/dev/null
-    #     status=$( echo $? )
-    #     if [[ $status == 0 ]] ; then
-    #         echo "Online"
+    for (( i=0; i<${RELAYS_COUNT}; i++ ));
+    do
+        echo "Checking ${RELAY_IPS[$i]} is online..."
+        ping -c1 -W1 -q ${RELAY_IPS[$i]} &>/dev/null
+        status=$( echo $? )
+        if [[ $status == 0 ]] ; then
+            echo "Online"
 
-    #         echo '---------------- Purging db... ----------------'
-    #         ssh -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]} 'rm -rf $HOME/node.relay/db/*; rm -rf $HOME/node.relay/logs'
-    #         echo '---------------- Backing up config files ----------------'
-    #         ssh -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]} 'mkdir $HOME/node.relay/config/bkup_respin; cp $HOME/node.relay/config/*.json $HOME/node.relay/config/bkup_respin'
+            echo '---------------- Purging db... ----------------'
+            ssh -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]} 'rm -rf $HOME/node.relay/db/*; rm -rf $HOME/node.relay/logs'
+            echo '---------------- Backing up config files ----------------'
+            ssh -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]} 'mkdir $HOME/node.relay/config/bkup_respin; cp $HOME/node.relay/config/*.json $HOME/node.relay/config/bkup_respin'
 
-    #         echo '---------------- Copying config files... ----------------'
-    #         mkdir -p $HOME/node.bp/config/config_${RELAY_NAMES[$i]}
-    #         rm -f $HOME/node.bp/config/config_${RELAY_NAMES[$i]}/*
-    #         scp -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]}:/home/cardano/node.relay/config/*.json $HOME/node.bp/config/config_${RELAY_NAMES[$i]}
-    #     else
-    #         echo "Offline"
-    #     fi
-    # done
+            echo '---------------- Copying config files... ----------------'
+            mkdir -p $HOME/node.bp/config/config_${RELAY_NAMES[$i]}
+            rm -f $HOME/node.bp/config/config_${RELAY_NAMES[$i]}/*
+            scp -i ~/.ssh/${RELAY_NAMES[$i]}.pem cardano@${RELAY_IPS[$i]}:/home/cardano/node.relay/config/*.json $HOME/node.bp/config/config_${RELAY_NAMES[$i]}
+        else
+            echo "Offline"
+        fi
+    done
 
     echo
     echo "Now please compare changes between config_master config files and live ones for each relay and leave in config_relay folders only the config files you want to promote to each live relay configuration."
@@ -149,49 +152,4 @@ if [[ $NODE_TYPE == "bp" ]]; then
     echo "You can now restart the relays."
     echo "You should start the bp in relay mode only by running $HOME/run.bp.respin.sh"
 
-elif [[ $NODE_TYPE == "relay" ]]; then
-    echo
-    echo '---------------- Purging db ----------------'
-    if ! promptyn "Proceed with purging the node's db? (y/n)"; then
-        echo "Ok bye for now!"
-        exit 1
-    fi
-
-    # purging the node's db
-    rm -rf $HOME/node.relay/db/*
-    rm -f $HOME/node.relay/cncli/cncli.db*
-    rm -rf $HOME/node.relay/logs
-    echo "DB purge done!"
-
-    echo
-    echo '---------------- Backing up config files ----------------'
-    if ! promptyn "Proceed with backing up config files? (y/n)"; then
-        echo "Ok bye for now!"
-        exit 1
-    fi
-
-    mkdir -p $HOME/node.relay/config/bkup_$NOW
-    cp $HOME/node.relay/config/*.json $HOME/node.relay/config/bkup_$NOW
-
-    mkdir -p $HOME/node.relay/config/config_master
-    rm -f $HOME/node.relay/config/config_master/*
-    cd $HOME/node.relay/config/config_master
-
-    wget -O config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/config.json
-    wget -O bgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/byron-genesis.json
-    wget -O sgenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/shelley-genesis.json
-    wget -O agenesis.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/alonzo-genesis.json
-    wget -O topology.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/topology.json
-    wget -O db-sync-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/db-sync-config.json
-    wget -O submit-api-config.json https://raw.githubusercontent.com/input-output-hk/cardano-world/master/docs/environments/preprod/submit-api-config.json
- 
-    echo "Config file backup done!"
-
-    echo
-    echo "Now please compare changes between config_master config files and live ones and promote manually the relevant changes to the config files in $HOME/node.relay/config."
-
-    if ! promptyn "Ready to proceed? (y/n)"; then
-        echo "Ok bye for now!"
-        exit 1
-    fi
 fi
