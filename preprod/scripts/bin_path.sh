03b43950de20f747949af3e0776df5b41d7f8143
Thomas Langlois <newriverhead75@gmail.com>
2023-11-21 11:48:00 +0000

    tweaks

--- preprod/scripts/bin_path.sh
+++ preprod/scripts/bin_path.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# if [[ $# -eq 1 && ! $1 == "" ]]; then BIN_NAME=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 {binaryName}"; exit 2; fi
-
-# echo "$(jq -r '."install-plan"[] | select(."component-name" == "exe:'$BIN_NAME'") | ."bin-file"' ~/download/cardano-node/dist-newstyle/cache/plan.json | head -n 1)"
-
 if [[ $# -eq 2 && ! $1 == "" && ! $2 == "" ]]; then 
     BIN_NAME=$1
     CARDANO_NODE_PATH=$2
