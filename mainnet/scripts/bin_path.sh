168e5e6397f4b6bfe64eb99ec118c1d0e705ebc1
Thomas Langlois <newriverhead75@gmail.com>
2023-12-07 15:23:10 +0000

    tweaks

--- mainnet/scripts/bin_path.sh
+++ mainnet/scripts/bin_path.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if [[ $# -eq 2 && ! $1 == "" && ! $2 == "" ]]; then 
+    BIN_NAME=$1
+    CARDANO_NODE_PATH=$2
+else 
+    echo -e "This script requires input parameters:\n\tUsage: $0 {binaryName} {cardanoNodePath}"
+    exit 2
+fi
 
-if [[ $# -eq 1 && ! $1 == "" ]]; then BIN_NAME=$1; else echo -e "This script requires input parameters:\n\tUsage: $0 {binaryName}"; exit 2; fi
+echo "$(jq -r '."install-plan"[] | select(."component-name" == "exe:'$BIN_NAME'") | ."bin-file"' $CARDANO_NODE_PATH/dist-newstyle/cache/plan.json | head -n 1)"
 
-echo "$(jq -r '."install-plan"[] | select(."component-name" == "exe:'$BIN_NAME'") | ."bin-file"' ~/download/cardano-node/dist-newstyle/cache/plan.json | head -n 1)"
