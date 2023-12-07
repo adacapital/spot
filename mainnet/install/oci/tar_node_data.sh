d3c8fbce0a92e0815c9e703d2ec5c91ba1547d21
Thomas Langlois <newriverhead75@gmail.com>
2023-12-07 17:22:51 +0000

    tweaks

--- mainnet/install/oci/tar_node_data.sh
+++ mainnet/install/oci/tar_node_data.sh
@@ -10,12 +10,14 @@
 # Initialize variables
 node_type=""
 root_path=""
+delta=false
 
 # Parse command line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -node_type) node_type="$2"; shift ;;
         -root_path) root_path="$2"; shift ;;
+        -delta) delta=true ;;
         *) echo "Unknown parameter: $1"; exit 1 ;;
     esac
     shift
@@ -23,15 +25,25 @@ done
 
 # Check if both arguments are provided
 if [ -z "$node_type" ] || [ -z "$root_path" ]; then
-    echo "Usage: $0 -node_type NODE_TYPE -root_path ROOT_PATH"
+    echo "Usage: $0 -node_type NODE_TYPE -root_path ROOT_PATH [-delta]"
     exit 1
 fi
 
-# Create the tar file
-tar -czvf "$root_path/node_archive.tar.gz" \
-    "$root_path/$node_type/" \
-    "$root_path/keys/" \
-    "$root_path/pool_keys/" \
-    "$root_path/pool_topology"
+if [ "$delta" = true ]; then
+    # Create the tar file in delta mode
+    tar -czvf "$root_path/node_archive_delta.tar.gz" \
+        "$root_path/$node_type/cncli/" \
+        "$root_path/$node_type/config/" \
+        "$root_path/$node_type/socket/" \
+        "$root_path/$node_type/"*.*
+    echo "Delta tar file created successfully at $root_path/node_archive_delta.tar.gz"
+else
+    # Create the full tar file
+    tar -czvf "$root_path/node_archive.tar.gz" \
+        "$root_path/$node_type/" \
+        "$root_path/keys/" \
+        "$root_path/pool_keys/" \
+        "$root_path/pool_topology"
+    echo "Full tar file created successfully at $root_path/node_archive.tar.gz"
+fi
 
-echo "Tar file created successfully at $root_path/node_archive.tar.gz"
