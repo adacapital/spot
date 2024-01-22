b3f50a8c60ae79c193c93016618e36ab37a829ea
Thomas Langlois <newriverhead75@gmail.com>
2024-01-22 11:56:41 +0000

    oci migration

--- preview/scripts/rotate_kes_keys.sh
+++ preview/scripts/rotate_kes_keys.sh
@@ -6,34 +6,78 @@
 NOW=`date +"%Y%m%d_%H%M%S"`
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 SPOT_DIR="$(realpath "$(dirname "$SCRIPT_DIR")")"
+PARENT1="$(realpath "$(dirname "$SPOT_DIR")")"
+ROOT_PATH="$(realpath "$(dirname "$PARENT1")")/preview"
 NS_PATH="$SPOT_DIR/scripts"
+TOPO_FILE=$ROOT_PATH/pool_topology
+
+
+echo "UPDATE POOL REGISTRATION STARTING..."
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+echo "SPOT_DIR: $SPOT_DIR"
+echo "ROOT_PATH: $ROOT_PATH"
+echo "NS_PATH: $NS_PATH"
+echo "TOPO_FILE: $TOPO_FILE"
 
 # importing utility functions
 source $NS_PATH/utils.sh
+
+echo
+echo '---------------- Reading pool topology file and preparing a few things... ----------------'
+
+read ERROR NODE_TYPE BP_IP RELAYS < <(get_topo $TOPO_FILE)
+RELAYS=($RELAYS)
+cnt=${#RELAYS[@]}
+let cnt1="$cnt/3"
+let cnt2="$cnt1 + $cnt1"
+let cnt3="$cnt2 + $cnt1"
+
+RELAY_IPS=( "${RELAYS[@]:0:$cnt1}" )
+RELAY_NAMES=( "${RELAYS[@]:$cnt1:$cnt1}" )
+RELAY_IPS_PUB=( "${RELAYS[@]:$cnt2:$cnt1}" )
+
+if [[ $ERROR == "none" ]]; then
+    if [[ $NODE_TYPE == "" ]]; then
+        echo "Node type not identified, something went wrong."
+        echo "Please fix the underlying issue and run init.sh again."
+        exit 1
+    else
+        echo "NODE_TYPE: $NODE_TYPE"
+        echo "RELAY_IPS: ${RELAY_IPS[@]}"
+        echo "RELAY_NAMES: ${RELAY_NAMES[@]}"
+            echo "RELAY_IPS_PUB: ${RELAY_IPS_PUB[@]}"
+    fi
+else
+    echo "ERROR: $ERROR"
+    exit 1
+fi
+
+NODE_PATH="$ROOT_PATH/node.bp"
 MAGIC=$(get_network_magic)
+echo "NODE_PATH: $NODE_PATH"
 echo "NETWORK_MAGIC: $MAGIC"
 
-cd $HOME
+cd $ROOT_PATH
 cd pool_keys
 
-if [ -f "$HOME/pool_keys/kes.skey" ]; then
+if [ -f "$ROOT_PATH/pool_keys/kes.skey" ]; then
     echo
     echo '---------------- Backing up previous KES key pair ----------------'
-    chmod 664 $HOME/pool_keys/kes.vkey
-    chmod 664 $HOME/pool_keys/kes.skey
-    mv $HOME/pool_keys/kes.vkey $HOME/pool_keys/kes.vkey.$NOW
-    mv $HOME/pool_keys/kes.skey $HOME/pool_keys/kes.skey.$NOW
-    chmod 400 $HOME/pool_keys/kes.vkey.$NOW
-    chmod 400 $HOME/pool_keys/kes.skey.$NOW
+    chmod 664 $ROOT_PATH/pool_keys/kes.vkey
+    chmod 664 $ROOT_PATH/pool_keys/kes.skey
+    mv $ROOT_PATH/pool_keys/kes.vkey $ROOT_PATH/pool_keys/kes.vkey.$NOW
+    mv $ROOT_PATH/pool_keys/kes.skey $ROOT_PATH/pool_keys/kes.skey.$NOW
+    chmod 400 $ROOT_PATH/pool_keys/kes.vkey.$NOW
+    chmod 400 $ROOT_PATH/pool_keys/kes.skey.$NOW
 fi
 
 
-if [ -f "$HOME/pool_keys/node.cert" ]; then
+if [ -f "$ROOT_PATH/pool_keys/node.cert" ]; then
     echo
     echo '---------------- Backing up previous operational certificate ----------------'
-    chmod 664 $HOME/pool_keys/node.cert
-    mv $HOME/pool_keys/node.cert $HOME/pool_keys/node.cert.$NOW
-    chmod 400 $HOME/pool_keys/node.cert.$NOW
+    chmod 664 $ROOT_PATH/pool_keys/node.cert
+    mv $ROOT_PATH/pool_keys/node.cert $ROOT_PATH/pool_keys/node.cert.$NOW
+    chmod 400 $ROOT_PATH/pool_keys/node.cert.$NOW
 fi
 
 
@@ -49,8 +93,8 @@ chmod 400 kes.skey
 echo
 echo '---------------- Generating the operational certificate ----------------'
 
-SLOTSPERKESPERIOD=$(cat $HOME/node.bp/config/sgenesis.json | jq -r '.slotsPerKESPeriod')
-CTIP=$(cardano-cli query tip --testnet-magic $MAGIC | jq -r .slot)
+SLOTSPERKESPERIOD=$(cat $ROOT_PATH/node.bp/config/sgenesis.json | jq -r '.slotsPerKESPeriod')
+CTIP=$(cardano-cli query tip --socket-path $ROOT_PATH/node.bp/socket/node.socket --testnet-magic $MAGIC | jq -r .slot)
 KES_PERIOD=$(expr $CTIP / $SLOTSPERKESPERIOD)
 echo "SLOTSPERKESPERIOD: $SLOTSPERKESPERIOD"
 echo "CTIP: $CTIP"
@@ -66,4 +110,4 @@ cardano-cli node issue-op-cert \
 echo
 echo '---------------- Restarting bp node ----------------'
 
-sudo systemctl restart run.bp
+sudo systemctl restart run.bp-preview.service
